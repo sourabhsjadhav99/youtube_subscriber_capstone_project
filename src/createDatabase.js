4b1f5e2fd8db7f75ba8cb404f0d53f91fb2aa2ff
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2024-04-03 17:01:19 +0530

    connected with mongoDb atlas

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
 const subscriberModel = require('./models/subscribersModel')
 const data = require('./data')
+const path = require("path")
+let dotenv=require('dotenv')
+
+dotenv.config({ path: '.env' })
+
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+// const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = process.env.MONGODB_URL
+
+mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true,  useNewUrlParser: true  });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('Database created...'))
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const path = require("path")
 const app = require('./app.js')
 const mongoose = require('mongoose')
 const port = 3000
+
+let dotenv=require('dotenv')
+dotenv.config({ })
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+// const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = process.env.MONGODB_URL;
+
+mongoose.connect(DATABASE_URL
+    ,{ useNewUrlParser: true, useUnifiedTopology: true,  useNewUrlParser: true  });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
